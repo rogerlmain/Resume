a318e329735bd2c72a448aef4e3c8ba88e8d5a36
Roger L. Main <roger.main@rexthestrange.com>
2022-04-28 12:11:38 -0600

    Updated to 2022

---
+++ Forms/clients/cosos.php
@@ -0,0 +1,19 @@
+<div class="popup-panel">
+
+	<h1>Colorado Secretary of State &dash; Denver, CO</h1>
+	<h2>Contract Programmer</h2>
+
+	<p>
+		I was mainly engaged in increasing the efficiency of their public self-service websites by analyzing and debugging legacy systems.
+	</p>
+	<p>
+		I was also involved in the development of a replacement ballot request process component in preparation for the last election as well
+		as adding features and enchancement to their notary registration website.
+	</p>
+
+	<div class="subnote">
+		<div class="header">Technologies:</div>
+		<div class="content">C#, VB.Net ASP.Net, Visual Basic 6, Classic ASP, SQL Server 2008</div>
+	</div>
+
+</div>
--- Forms/clients/doi.php
+++ Forms/clients/doi.php
@@ -2,23 +2,23 @@
 
 	<h1>United States Department of the Interior (Mindbank Consulting)</h1>
 	<h2>Contract Programmer</h2>
-	
+
 	<p>
-		Designed and developed an internal Cloud Services request intranet system including email forwarding and custom content management.
+		For internal efficiency, I designed and developed an internal Cloud Services request intranet system including email forwarding and custom content management.
 	</p>
 	<p>
-		Designed and developed an internal Service Catalog intranet system for employee self-service.
+		I also designed and developed a catalog intranet system for employee self-service, helping to reduce tech support load.
 	</p>
 	<p>
-		Designed and developed web services using the latest Java Development Kit for communications with a MySQL database for added security for the aforementioned internal web applications.
+		In addition, I designed and developed web services using the latest Java Development Kit for communications with a MySQL database for added security for the aforementioned internal web applications.
 	</p>
 	<p>
-		Incorporated a SimpleSAMLphp interface for single sign-on to the aforementioned internal web applications as well as set up and administered a Linux Apache hosting system.
+		To increase convenience and security, I incorporated a SimpleSAMLphp interface for single sign-on to their internal web applications as well as set up and administered a Linux Apache hosting system.
 	</p>
-	
+
 	<div class="subnote">
 		<div class="header">Technologies:</div>
 		<div class="content">PHP Version 5.6.10, FuelPHP Version 1.7.3, MySQL Version 5.6, SimpleSAMLphp Version 1.13, jQuery Version 1.11, JDK 1.8</div>
 	</div>
-		
+
 </div>
